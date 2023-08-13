c9508d95f6e6d75b67d0e4c81ca9e4217a2d9f6a
dimb12 <128401417+dimb12@users.noreply.github.com>
2023-08-13 18:50:33 +0300

    r
    gr

--- While.java
+++ While.java
@@ -1,18 +1,33 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class While {
     public static void main(String[] args) {
+        Random random = new Random();
+        Set<Integer> numbers = new HashSet<>();
 
+<<<<<<< Updated upstream
         // int i = 2;
         // while (i <= 8) {
         //    System.out.print(i + " ");
         //    i+=2; //i=i+2
         // }
+=======
+        // Генерация 6 уникальных чисел
+        while (numbers.size() < 6) {
+            int randomNumber = random.nextInt(34) + 1;
+            numbers.add(randomNumber);
+        }
+>>>>>>> Stashed changes
 
-        // int i = 1;
-        // while (i < 0) {
-        //     System.out.print(i + " ");
-        //     i++;
-        // }
+        // Вывод сгенерированных чисел
+        System.out.println("Сгенерированные числа:");
+        for (int number : numbers) {
+            System.out.println(number);
+        }
 
+<<<<<<< Updated upstream
         // int i = 1;
         // while (true) { //while(i > 0)
         //     System.out.print(i + " ");
@@ -37,5 +52,12 @@ public class While {
         //    i++;
         // }
 
+=======
+        // Генерация дополнительного числа от 1 до 7
+        int additionalNumber = random.nextInt(7) + 1;
+
+        // Вывод дополнительного числа
+        System.out.println("Дополнительное число: " + additionalNumber);
+>>>>>>> Stashed changes
     }
 }
---
+++ test1.py
@@ -0,0 +1,4 @@
+imya = input("Введите имя: ")
+number = int(input("Год рождения: "))
+actual = 2023 - number
+print(imya + ", ваш возраст: " + str(actual) + " лет.")
